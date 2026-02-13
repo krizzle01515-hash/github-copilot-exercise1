9d393a7d2211187e64959f43add3f51f23a9d45c
krizzle01515-hash <krizzle01515@gmail.com>
2026-02-13 07:39:42 +0000

    Fix formatting of activities dictionary initialization for consistency

--- src/app.py
+++ src/app.py
@@ -21,8 +21,7 @@ app.mount("/static", StaticFiles(directory=os.path.join(Path(__file__).parent,
 
 # In-memory activity database 
 
-activities = 
-    {
+activities = {
         "Chess Club": {
             "description": "Learn strategies and compete in chess tournaments",
             "schedule": "Fridays, 3:30 PM - 5:00 PM",
